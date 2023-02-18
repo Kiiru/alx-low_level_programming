4ae91d5e4f2f57d45e5cc41f04c4f64675121abc
Kiiru <mwangi.ramson@gmail.com>
2023-02-18 05:32:20 -0800

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * main: entry
+ *
+ * Description: printf
+ *
+ * return: 0
+ */
+
+int main(void)
+{
+	char text[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf("%s", text);
+	return (0);
+}
