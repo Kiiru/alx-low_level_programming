0e313932fda574a1567ba9e4441f57fb3c5091ac
Kiiru <mwangi.ramson@gmail.com>
2023-02-19 23:52:42 -0800

    101-quote.c output with new line

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,6 @@
 int main(void)
 {
 	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59, 1, stderr);
-	fwrite("\n", sizeof(char), 1, stderr);
 	exit(EXIT_FAILURE);
 	return (1);
 }
