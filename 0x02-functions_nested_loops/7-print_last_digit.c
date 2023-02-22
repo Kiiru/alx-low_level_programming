abfb5adcc6ac8fd43b5160a4c4a1482f89e4af6c
Kiiru <mwangi.ramson@gmail.com>
2023-02-22 07:09:33 -0800

    append last digit to remainder

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_last_digit - gets the last digit of int n
@@ -8,6 +9,8 @@
 int print_last_digit(int n)
 {
 	int remainder = n % 10;
+
+	_putchar(remainder + '0');
 
 	return (remainder);
 }
