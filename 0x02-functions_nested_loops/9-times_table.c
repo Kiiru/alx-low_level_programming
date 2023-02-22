cd3baeb22734bbc316b48cb0d06bc58e7bed93a8
Kiiru <mwangi.ramson@gmail.com>
2023-02-22 08:41:10 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - computes the time table
+ *
+ * Returns: void
+ */
+void times_table(void)
+{
+	int i = 0, j;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			int t = i * j;
+
+			if (t == 0 && j == 0)
+				printf("");
+			if (t < 10 && t > 0)
+				printf("  ");
+			else
+				printf(" ");
+			printf(",%d", t);
+			j++;
+		}
+		printf("\n")
+		i++;
+	}
+}
