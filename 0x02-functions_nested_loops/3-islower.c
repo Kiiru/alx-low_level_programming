00590b38f52cdefda4379c1a2254d1cd12bf9777
Kiiru <mwangi.ramson@gmail.com>
2023-02-22 05:26:38 -0800

    3-islower solve betty linter errors

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,14 @@
 /**
  * _islower - procedure to check case of a char
  *
- * Return: Always void
+ *@c: char input
+ *
+ * Return: return 1 if is lowercase else 0 
  */
 int _islower(int c)
 {
 	if (c < 123 && c > 96)
-		return 1;
+		return (1);
 		
-	return 0;
+	return (0);
 }
