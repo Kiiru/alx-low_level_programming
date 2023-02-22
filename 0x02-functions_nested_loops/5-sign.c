22de752af05f3f2ca34d2253fb2aa7205cd6f453
Kiiru <mwangi.ramson@gmail.com>
2023-02-22 06:05:38 -0800

    5-sign append sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_sign - procedure to check if a char is an alphabet
@@ -8,8 +9,11 @@
 int print_sign(int n)
 {
 	if (n > 0)
+		_putchar('+');
 		return (1);
 	else if (n == 0)
+		_putchar('0')
 		return (0);
+	_putchar('-');
 	return (-1);
 }
