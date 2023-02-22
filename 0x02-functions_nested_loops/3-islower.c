8c923a46914842946f1a1c5ea1842098144b5868
Kiiru <mwangi.ramson@gmail.com>
2023-02-22 05:34:51 -0800

    Betty trailing space fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,15 +2,12 @@
 
 /**
  * _islower - procedure to check case of a char
- *
- *@c: char input
- *
- * Return: return 1 if is lowercase else 0 
+ * @c: char input
+ * Return: return 1 if is lowercase else 0
  */
 int _islower(int c)
 {
 	if (c < 123 && c > 96)
 		return (1);
-		
 	return (0);
 }
