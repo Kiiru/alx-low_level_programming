5d290f7fadcefc4c60afd2b9dc3c9953b7c73846
Kiiru <mwangi.ramson@gmail.com>
2023-02-23 01:06:06 -0800

    10-add.c betty coding syle

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,13 @@
 
 /**
  * add - add two integers
- *
+ * @a: first int
+ * @b: second int
  * Return: the result of the addition
  */
 int add(int a, int b)
 {
 	int c = a + b;
+
 	return (c);
 }
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ */
+void print_to_98(int n)
+{
+}
