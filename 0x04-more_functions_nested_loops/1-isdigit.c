6ada2e66307d22cead45e7fad544dcb8684023da
Kiiru <mwangi.ramson@gmail.com>
2023-02-23 08:50:08 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _isdigit - procedure to check if a value is a digit btwn 0 - 9
+ * @c: input digit
+ * Return: return 1 if its a digit and 0 if not
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	return (0);
+}
