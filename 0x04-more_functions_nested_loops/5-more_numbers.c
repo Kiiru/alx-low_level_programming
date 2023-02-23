b106a9342f835f367912c07b778d7b42201a1e6a
Kiiru <mwangi.ramson@gmail.com>
2023-02-23 12:13:53 -0800

    5-more_numbers_more _problems

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,9 +16,11 @@ void more_numbers(void)
 		j = 0;
 		while (j < 15)
 		{
-			_putchar(j + '0');
+			if (j > 9)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 			j++;
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
