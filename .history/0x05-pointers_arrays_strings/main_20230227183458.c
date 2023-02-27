c40b4e567802a57579a20c426e34359d51c8c8cc
Kiiru <mwangi.ramson@gmail.com>
2023-02-27 18:56:10 +0300

    reset_to_98

---
+++ .history/0x05-pointers_arrays_strings/main_20230227183458.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    in
+}
---
+++ .history/0x05-pointers_arrays_strings/main_20230227183555.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+
+    n = 402;
+    printf("n=")
+}
