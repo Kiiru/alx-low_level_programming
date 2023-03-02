bb1d443626b83530b81585b2df550aab61ee4d33
Kiiru <mwangi.ramson@gmail.com>
2023-03-02 14:35:23 +0300

    100-rot13.c

---
+++ .history/0x06-pointers_arrays_strings/100-rot13_20230302140248.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rot13 - operation to encode string
+ * @s: pointer to string array
+ * Return: always encoded String
+ *
+*/
+char *rot13(char *s)
+{
+	int i, k;
+
+
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			k = (((s[i] - 'a') + 13) % 26) + 'a';
+			s[i] = k;
+		}
+		else if (s[i] >= 'A' && s[i] <= 'Z')
+		{
+			k = (((s[i] - 'A') + 13) % 26) + 'A';
+			s[i] = k;
+		}
+	}
+
+	return (s);
+}
