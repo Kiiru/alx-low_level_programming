2fdcfaf1fe114598aa508f2c40e116e0b296970b
Kiiru <mwangi.ramson@gmail.com>
2023-03-06 18:06:05 +0300

    2-strchr.c

--- .history/0x07-pointers_arrays_strings/2-strchr_20230306180444.c
+++ .history/0x07-pointers_arrays_strings/2-strchr_20230306180444.c
@@ -10,12 +10,14 @@
 */
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i ;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s != '\0')
 	{
 		if (s[i] == c)
 			return (&s[i]);
+		s++;
+		i++;
 	}
 	return (0);
 }
--- .history/0x07-pointers_arrays_strings/2-strchr_20230306180446.c
+++ .history/0x07-pointers_arrays_strings/2-strchr_20230306180446.c
@@ -12,10 +12,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s != '\0')
 	{
 		if (s[i] == c)
 			return (&s[i]);
+		s++;
+		i++;
 	}
 	return (0);
 }
