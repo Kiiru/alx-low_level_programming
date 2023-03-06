96037fa6aef882a2fc8f952bc969155166c6cb2c
Kiiru <mwangi.ramson@gmail.com>
2023-03-06 18:59:26 +0300

    4-strbrk.c

---
+++ .history/0x07-pointers_arrays_strings/4-strpbrk_20230306185615.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strspn - operation to find c in s
+ * @s: pointer to string array
+ * @accept: char
+ * Return: always pointer to matching char
+ *
+*/
---
+++ .history/0x07-pointers_arrays_strings/4-strpbrk_20230306185639.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+
+/**
+ * _strspn - operation to find c in s
+ * @s: pointer to string array
+ * @accept: char
+ * Return: always pointer to matching char
+ *
+*/
