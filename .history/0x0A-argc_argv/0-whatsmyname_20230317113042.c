d68ab2aae348fb976809cbb7c575286276a9c5fd
Kiiru <mwangi.ramson@gmail.com>
2023-03-17 11:32:04 +0300

    0-whatsmyname.c

---
+++ .history/0x0A-argc_argv/0-whatsmyname_20230317113042.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * 
+ * 
+*/
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+
+	return (EXIT_SUCCESS);
+}
---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+
+	return (EXIT_SUCCESS);
+}
