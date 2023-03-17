fccfbda9afcfbaec707181ec8d5ccf990dc339b8
Kiiru <mwangi.ramson@gmail.com>
2023-03-17 11:51:04 +0300

    1-args.c

---
+++ .history/0x0A-argc_argv/1-args_20230317114940.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: size of arguments
+ * @argv: pointer to array of arguments
+ * Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+		printf("%d\n", argc - 1);
+
+	return (EXIT_SUCCESS);
+}
