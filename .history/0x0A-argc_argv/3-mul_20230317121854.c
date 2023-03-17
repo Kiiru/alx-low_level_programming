747b542399df0b38efe73c29c25cb07bf398af46
Kiiru <mwangi.ramson@gmail.com>
2023-03-17 12:22:36 +0300

    3-mul.c

---
+++ .history/0x0A-argc_argv/3-mul_20230317121854.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: size of arguments
+ * @argv: pointer to array of arguments
+ * Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	int r;
+
+	if ((argc) - 1 == 2)
+	{
+		printf("%d\n", ((int) argv[1] * (int) argv[2])
+	}
+}
