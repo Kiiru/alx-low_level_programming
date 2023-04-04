8f6704925741d73afdab5818cb8bf2f7ba7d3e12
Kiiru <mwangi.ramson@gmail.com>
2023-04-04 14:21:00 +0300

    1-list_len.c

---
+++ .history/0x12-singly_linked_lists/1-list_len_20230404141758.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * list_len - Operation to print all the nodes of a list
+ * @h: list of nodes list_t
+ * Return: number of nodes in the list
+*/
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+	while
+}
