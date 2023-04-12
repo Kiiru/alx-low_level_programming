703d857914ed8422ed0770eccf8374366e55512d
Kiiru <mwangi.ramson@gmail.com>
2023-04-12 13:10:44 +0300

    2-get_bit.c

---
+++ .history/0x14-bit_manipulation/2-get_bit_20230412130826.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * get_bit - Operation to return bit at a specified index
+ * @n
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	return ((index > (8 * sizeof(n) - 1)) ? -1 : (int)(n >> index) & 1);
+}
---
+++ .history/0x14-bit_manipulation/2-get_bit_20230412130905.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * get_bit - Operation to return bit at a specified index
+ * @n: long int value
+ * @index: int index
+ * Return: int bit or -1 
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	return ((index > (8 * sizeof(n) - 1)) ? -1 : (int)(n >> index) & 1);
+}
